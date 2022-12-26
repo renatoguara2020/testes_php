7ba223a763b1a95bca04ab05c0d919127c2c3c8e
Renato Alves Soares <renatoguara2020@gmail.com>
2022-12-25 21:16:50 -0300

    ok

--- testes_php.php
+++ testes_php.php
@@ -28,3 +28,28 @@ try {
 
 $conn = null;
 ?>
+
+
+#########################################################################################$_COOKIE
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email))");
+  // use exec() because no results are returned
+  $stmt->bindValue(':firstname',$firstname, PDO::PARAM_STR);
+  $conn->exec($sql);
+  echo "New record created successfully";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
