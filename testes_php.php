88a2f212f0ab99dda4eb2add68e25e7824b33a31
Renato Alves Soares <renatoguara2020@gmail.com>
2022-12-25 21:35:23 -0300

    ok

--- testes_php.php
+++ testes_php.php
@@ -42,9 +42,11 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email))");
+  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
   // use exec() because no results are returned
   $stmt->bindValue(':firstname',$firstname, PDO::PARAM_STR);
+  $stmt->bindValue(':lastname',$lastname, PDO::PARAM_STR);
+  $stmt->bindValue(':email',$email, PDO::PARAM_STR);
   $conn->exec($sql);
   echo "New record created successfully";
 } catch(PDOException $e) {
